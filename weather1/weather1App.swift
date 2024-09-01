f95626761a6c214e6bcf049990f5957523a824d7
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2024-09-01 18:43:43 -0500

    commit 1: Hello, world! SwiftUI_IOS

---
+++ weather1/weather1App.swift
@@ -0,0 +1,17 @@
+//
+//  weather1App.swift
+//  weather1
+//
+//  Created by Nziemaraang Nziengui Boussougou on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct weather1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
